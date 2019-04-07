e5de23751e7429db5298d3fddf25a2caece6b848
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-04-07 09:57:16 -0300

    Ajustes finais nas funçoes.

--- Atividade 01/exercicio2.c
+++ Atividade 01/exercicio2.c
@@ -1,65 +1,83 @@
 #include<stdio.h>
-#define QUANTIDADE 2
+#define QUANTIDADE 10
 
-int recebe_notas(float *NOTAS, int num);
+int *recebe_notas(float *NOTAS, int num, int *apr);
+void conta_notas(int *APR,int num, int *num_aprovados, int *num_reprovados);
+int percent_aprov(int num_aprovados, int num_reprovados,float *prec_aprovados, float *perc_reprovados);
+
 
 int main()
-{   float NOTAS[10];
-    int num_aprovados;
-    printf("Por favor informe as 10 notas:");
-    for( int i = 0; i < QUANTIDADE; i++)
-    {
-        scanf("%f",NOTAS);
-    }
+{   float NOTAS[QUANTIDADE];
+    float prec_aprovados, perc_reprovados;
+    int num_aprovados, num_reprovados;
+    int APR[QUANTIDADE], *apr, mais_metade;
+   
     
 
-    printf("Quantidade de aprovados:");
-    printf("Quantidade de reprovados:");
-    printf("Percentual de aprovados:");
-    printf("Percentual de reprovados:");
-    printf("mais da metade da turma aprovada:");
+    printf("Por favor informe as 10 notas:\n");
+    for( int i = 0; i < QUANTIDADE; i++)
+    {
+        scanf("%f",&NOTAS[i]);
+    }
+    
+    apr = recebe_notas(NOTAS,QUANTIDADE,APR);
+    conta_notas(apr, QUANTIDADE,&num_aprovados,&num_reprovados);
+    mais_metade = percent_aprov(num_aprovados,num_reprovados,&prec_aprovados, &perc_reprovados);
+
+   
+    printf("Quantidade de aprovados: %d \n",num_aprovados);
+    printf("Quantidade de reprovados: %d \n",num_reprovados);
+    printf("Percentual de aprovados: %.2f \n", prec_aprovados);
+    printf("Percentual de reprovados: %.2f \n", perc_reprovados);
+    printf("Mais da metade da turma aprovada: %s \n", mais_metade == 1 ?  "SIM" : "NAO");
     
     return 0;
 }
 // ========== FUNÇÕES =============
 
-int recebe_notas(float *NOTAS, int num){
-    int APR[QUANTIDADE];
+int *recebe_notas(float *NOTAS, int num,int *apr){
+    
     for(int i = 0; i < num; i++)
     {
         if (*(NOTAS+i) >= 6.0 ) {
-            APR[i] = 1;
+            *(apr + i) = 1;
         }else
         {
-            APR[1]= 0;
+            *(apr + i)= 0;
         }
+        
     }
-    return APR;
+    return apr;
 
 
 }
 
 void conta_notas(int *APR,int num, int *num_aprovados, int *num_reprovados){
+    *num_aprovados = 0;
+    *num_reprovados = 0;
     for(size_t i = 0; i < num; i++)
     {
-       if (*(APR + 1) == 1) {
-           *num_aprovados =+ 1;
+        
+       if (*(APR + i) == 1) {
+           *num_aprovados += 1;
            
        }else
        {
-           num_reprovados =+ 1;
+           *num_reprovados += 1;
+           
        }
        
        
     }
 }
 
-int percent_aprov(int *num_aprovados, int *num_reprovados,float *prec_aprovados, float *perc_reprovados){
+int percent_aprov(int num_aprovados, int num_reprovados,float *prec_aprovados, float *perc_reprovados){
 
-int total = *num_aprovados + num_reprovados;
+float total = num_aprovados + num_reprovados;
 
-prec_aprovados = (*num_aprovados/total)*100;
-perc_reprovados = (*num_reprovados/total)*100;
+
+*prec_aprovados = (num_aprovados/total)*100;
+*perc_reprovados = (num_reprovados/total)*100;
 
     if ((*prec_aprovados) >= (*perc_reprovados)){
         return 1;
