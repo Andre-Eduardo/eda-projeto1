ee2c5c8de28fd01a35fce106600d18cdb402e735
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-06-02 14:44:45 -0300

    inicializando os valores :)

--- Atividade 04/main.c
+++ Atividade 04/main.c
@@ -11,43 +11,83 @@ Hugo Aragão de Oliveira - 16/0124581
 
 #include <stdio.h>
 #include "fila.h"
+#include <math.h>
+#include <time.h>
+#define UNI_TIME 5 // unidade de tempo 1 uma unidade = 5 mim
+#define MAX_AVI 64 // maximo de avioes
+#define MIM_AVI 20 // mminimo de avioes
 
-void initRelogio(char *relogio);
-void incRelogio(char *relogio);
 
-int main(){
+void inic_alatorio( int *NVoos, int* NAproximacoes,int* NDecolagens);
+
+int main()
+{
   //Declarações:
-  int i;
-	char relogio[6];
-  //Instruções:
-  
+  char* cod_Voos[64] = {"VG3001", "JJ4404", "LN7001", "TG1501", "GL7602", "TT1010", "AZ1009",
+                       "AZ1008", "AZ1010", "TG1506", "VG3002", "JJ4402", "GL7603", "RL7880",
+                       "AL0012", "TT4544", "TG1505", "VG3003", "JJ4403", "JJ4401", "LN7002",
+                       "AZ1002", "AZ1007", "GL7604", "AZ1006", "TG1503", "AZ1003", "JJ4403",
+                       "AZ1001", "LN7003", "AZ1004", "TG1504", "AZ1005", "TG1502", "GL7601",
+                       "TT4500", "RL7801", "JJ4410", "GL7607", "AL0029", "VV3390", "VV3392",
+                       "GF4681", "GF4690", "AZ1020", "JJ4435", "VG3010", "LF0920", "AZ1065",
+                       "LF0978", "RL7867", "TT4502", "GL7645", "LF0932", "JJ4434", "TG1510",
+                       "TT1020", "AZ1098", "BA2312", "VG3030", "BA2304", "KL5609", "KL5610",
+                       "KL5611"};
 
+  int NVoos; // numero de voos (20 ate 64) NVoos = NAproximaçoes + NDecolagens
+  int NAproximacoes; // numero de avioes querendo pousar (10 32)
+  int NDecolagens; // avioes querendo decolar (10 a 32)
+ 
+  fila oi;
+  data ale;
+  list_enc bagaca;
+  int i;
+  //Instruções:
+ 
+  // ale.nome[0] = 'a';
+  // ale.nome[1] = '\0';
+
+  // oi = criaFilaVazia();
+  // oi = pushF(oi, ale);
+  // oi = pushF(oi, ale);
+  // oi = pushF(oi, ale);
+  // ale.nome[0] = 'a';
+  // ale.nome[1] = 'b';
+  // ale.nome[2] = 'a';
+  // ale.nome[3] = '\0';
+  // oi = pushF(oi, ale);
+  // bagaca = *(oi.fim);
+  // printf("%s\n", bagaca.dados.nome);
+  // bagaca = *(oi.ini);
+  // printf("%s\n", bagaca.dados.nome);
+  // bagaca = *(bagaca.prox);
+  // printf("%s\n", bagaca.dados.nome);
+  // bagaca = *(bagaca.prox);
+  // printf("%s\n", bagaca.dados.nome);
+  // bagaca = *(bagaca.prox);
+  // printf("%s\n", bagaca.dados.nome);
+  // printf("%d\n", tamanhoFila(oi));
+  // ale = popF(&oi);
+  // printf("%s\n", ale.nome);
+  // ale = popF(&oi);
+  // printf("%s\n", ale.nome);
+  // ale = popF(&oi);
+  // printf("%s\n", ale.nome);
+  // ale = popF(&oi);
+  // printf("%s\n", ale.nome);
+  // printf("%p\n", oi.fim);
+
+  // oi = liberaFila(oi);
+inic_alatorio( &NVoos, &NAproximacoes,&NDecolagens);
+printf("total =%d\n",NVoos);
+printf("aprox =%d\n",NAproximacoes);
+printf("decola =%d\n",NDecolagens);
   return 0;
 }
+ void inic_alatorio( int *NVoos, int* NAproximacoes,int* NDecolagens){
+  srand(time(NULL));
 
-void initRelogio(char *relogio){
-	strcpy(relogio, "23:52");
-
-	return;
-}
-
-void incRelogio(char *relogio){
-	int hora=0;
-	int minutos;
-
-	minutos=*(relogio+4)-'0';
-	minutos+=(*(relogio+3)-'0')*10;
-	hora=*(relogio+1)-'0';
-	hora+=(*(relogio)-'0')*10;
-
-	if(minutos>=55)
-		hora=(hora+1)%24;
-	minutos=(minutos+5)%60;
-
-	relogio[0]=hora/10+'0';
-	relogio[1]=hora%10+'0';
-	relogio[3]=minutos/10+'0';
-	relogio[4]=minutos%10+'0';
-
-	return;
+  *NAproximacoes = (rand() % 32 )+10;
+  *NDecolagens = (rand()%(62 - *NAproximacoes )) +10;
+  *NVoos = *NAproximacoes + *NDecolagens;
 }
