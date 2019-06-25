54e87b8e70c96e7a72086aa1a5ef291314388c30
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-06-25 12:23:35 -0300

    ;-)

--- Atividade 06/funcoes.h
+++ Atividade 06/funcoes.h
@@ -2,6 +2,14 @@
 #define _FUNCOES_
 
 #include<math.h>
+#include<stdlib.h>
 #define VET_FEQ 526
+
+typedef struct neuronio
+{
+   double *pesos;
+   double *deslocamento;
+   
+}Neuronio;
 
 #endif 
--- Atividade 06/main.c
+++ Atividade 06/main.c
@@ -1,6 +1,13 @@
 #include "funcoes.h"
+#include <stdio.h>
 int main(int argc, char const *argv[])
-{
+
+{   if(argc > 1){
+    int num_camada_oculta = atoi(argv[1]);
+}else{
+    puts("inicialize a camada oculta");
+    return 1;
+}
     
     return 0;
 }
