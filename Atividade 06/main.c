e295bc0ace8835d80f7b10c55aaa3060ba3bd1ed
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-06-26 17:12:25 -0300

    oloquinho meu

--- Atividade 06/main.c
+++ Atividade 06/main.c
@@ -1,12 +1,20 @@
 #include "funcoes.h"
 #include "identificador_de_solo.h"
 #include <stdio.h>
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
+    //Declarações
+    int num_camada_oculta;
+    double* vet_feat_grama;
+    double* vet_feat_asfalto; 
+    double* vet_feat_grama_teste;
+    double* vet_feat_asfalto_teste;
+    Neuronio *n,n2,n_f;
+    //Instruções
+
     srand(time(NULL));
     if (argc > 1)
     {
-        int num_camada_oculta = atoi(argv[1]);
+        num_camada_oculta = atoi(argv[1]);
     }
     else
     {
@@ -14,8 +22,14 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
+
+
+    n = inicializa_neuronio(VET_FEQ);// quantidade da primera camada/
+    n2 = inicializa_neuronio(num_camada_oculta); // numero de neuronios na camada oculta;
+    n_f = inicializa_neuronio(1); // ultima camada da rede neural
+    
     // teste
-    Neuronio *n;
+   
     n = inicializa_neuronio(4);
     for (int i = 0; i < 4; i++)
     {
